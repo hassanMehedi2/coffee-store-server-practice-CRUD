d7981ef92dd2800d6c7503899bad623ff3a41592
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2024-02-14 03:15:38 +0600

    added delete operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors'); 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config()
 const app= express();
 const port = process.env.PORT || 5000
@@ -40,10 +40,14 @@ async function run() {
         const newCoffee = req.body;
         const result = await coffeeCollection.insertOne(newCoffee)
         res.send(result)
-
-
     })
 
+    app.delete('/coffee/:id',async(req,res)=>{
+        const id = req.params;
+        const query = {_id : new ObjectId (id)}
+        const result = await coffeeCollection.deleteOne(query)
+        res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
