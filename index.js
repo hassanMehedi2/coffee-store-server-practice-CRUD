aa266a837b691a5dc8ad70e0965e88208506ce8d
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2024-02-14 01:52:14 +0600

    added post

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 // database
@@ -28,6 +28,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const coffeeCollection = client.db('coffeeDB').collection("coffee")
+
+    app.post('/coffee',async(req,res)=>{
+        const newCoffee = req.body;
+        const result = await coffeeCollection.insertOne(newCoffee)
+        res.send(result)
+
+
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
