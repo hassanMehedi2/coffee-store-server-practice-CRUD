632388c5aa9bc1745b4c12de1aeae0126f144f68
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2024-02-14 19:53:51 +0600

    user last login in time updated in database while login

--- index.js
+++ index.js
@@ -91,6 +91,24 @@ async function run() {
       res.send(result);
     })
 
+    app.delete(`/user/:id`,async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+    app.patch('/user',async(req,res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      const updatedUser = {
+        $set :{
+          lastLoggedAt : user.lastLoggedAt
+        }
+      }
+      const result = await usersCollection.updateOne(query,updatedUser);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
